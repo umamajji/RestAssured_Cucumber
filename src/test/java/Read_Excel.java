63b55cf48cd6273f70ee5d959abed7912097974f
Uma majji <ukumari@nisum.com>
2019-11-11 15:52:56 +0530

    REST ASSURED

---
+++ src/test/java/Read_Excel.java
@@ -0,0 +1,11 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+
+public class Read_Excel {
+    @Given("^I login with the guest user$")
+    public void iLoginWithTheGuestUser() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new PendingException();
+
+    }
+}
